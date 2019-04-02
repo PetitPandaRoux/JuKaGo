c0d0581afe25cbf1a03f87398d58e8e2176485a5
kw-andy <andy.kwok.work@gmail.com>
2019-04-02 22:54:15 +0200

    adding some views

--- jeux/admin.py
+++ jeux/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from jeux.models import Jeu
+from jeux.models import Jeu,Regle,Piece,Createur
 
 # Register your models here.
 
 admin.site.register(Jeu)
+admin.site.register(Regle)
+admin.site.register(Piece)
+admin.site.register(Createur)
--- jeux/models.py
+++ jeux/models.py
@@ -13,33 +13,40 @@ PIECE_POSSIBLE = (
 )
 
 class Piece(models.Model):
+    def __str__(self):
+        return self.type_piece
     type_piece = models.CharField(max_length=8,choices=PIECE_POSSIBLE,default='Autre')
     description = models.TextField()
     previsualisation = models.TextField()
     url_telechargement = models.CharField(max_length=255)
 
 class Regle(models.Model):
+    def __str__(self):
+        return self.materiels
     materiels = models.TextField()
     tour_de_jeu = models.TextField()
     annexes = models.TextField()
 
 
 class Jeu(models.Model):
+    def __str__(self):
+        return self.titre
     class Meta:
         verbose_name_plural = 'jeux'
     titre = models.CharField(max_length=255)
     type_jeu = models.CharField(max_length=255)
     description = models.TextField()
-    regle_id = models.ForeignKey(Regle,on_delete=models.CASCADE)
+    regle = models.ForeignKey(Regle,on_delete=models.CASCADE)
     popularite = models.PositiveSmallIntegerField()
     date_creation = models.DateField()
     version = models.PositiveSmallIntegerField()
     picture_url = models.CharField(max_length=255)
     picture_in_game_url = models.CharField(max_length = 255)
+    auteur = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
 
 class Alter_Jeu(models.Model):
     description = models.TextField()
-    regle_id = models.ForeignKey(Regle,on_delete=models.CASCADE)
+    regle = models.ForeignKey(Regle,on_delete=models.CASCADE)
     popularite = models.PositiveSmallIntegerField()
     date_creation = models.DateField()
     version = models.PositiveSmallIntegerField()
@@ -48,8 +55,8 @@ class Alter_Jeu(models.Model):
 
 
 class Piece_jeu(models.Model):
-    piece_id = models.ForeignKey(Piece,on_delete=models.CASCADE)
-    jeu_id = models.ForeignKey(Jeu,on_delete=models.CASCADE)
+    piece = models.ForeignKey(Piece,on_delete=models.CASCADE)
+    jeu = models.ForeignKey(Jeu,on_delete=models.CASCADE)
 
 
 
@@ -64,12 +71,14 @@ ROLE_POSSIBLE = (
 
 
 class Createur(models.Model):
-    auteur_id = models.ForeignKey(User,on_delete=models.CASCADE)
-    jeu_id = models.ForeignKey(Jeu,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.auteur
+    auteur = models.ForeignKey(User,on_delete=models.CASCADE)
+    jeu = models.ForeignKey(Jeu,on_delete=models.CASCADE)
     role = models.CharField(max_length=15,choices=ROLE_POSSIBLE,default='Créateur')
 
 class Commentaire(models.Model):
-    jeu_id = models.ForeignKey(Jeu,on_delete=models.SET('Commentaire effacé'))
+    jeu = models.ForeignKey(Jeu,on_delete=models.SET('Commentaire effacé'))
     commentaire = models.TextField()
     auteur = models.ForeignKey(Createur,on_delete=models.SET('Commentaire effacé'))
 
